d84441cca3f4611cff9ab1d85565d80ba6bb8ffe
bongjoonkim <bongjoonkim96@gmail.com>
2025-02-14 01:54:20 +0900

    prettier 적용 중

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -5,7 +5,6 @@ import { getFilteredEvents } from '../utils/eventUtils';
 
 export const useSearch = (events: Event[], currentDate: Date, view: 'week' | 'month') => {
   const [searchTerm, setSearchTerm] = useState('');
-
   const filteredEvents = useMemo(() => {
     return getFilteredEvents(events, searchTerm, currentDate, view);
   }, [events, searchTerm, currentDate, view]);
